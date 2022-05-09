a717f4383917bced14781a0bedb2bc93b3b045ff
bjpc <brandonpineda4@gmail.com>
2022-05-09 17:41:23 -0600

    :bug: [FIX] Header, My order, Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <>
       <nav className={styles.Nav}>
-        <Image src={menu} alt="menu" className={styles.menu} />
+        <img src={menu} alt="menu" className={styles.menu} />
         <div className={styles['navbar-left']}>
           <Link href="/">
             <Image src={logo} alt="logo" className={styles['nav-logo']} />
@@ -43,10 +43,16 @@ const Header = () => {
         </div>
         <div className={styles['navbar-right']}>
           <ul>
-            <li className={`${styles['more-clickable-area']} ${styles['navbar-email']} ${styles.pointer}`} onClick={() => {}} onKeyPress={() => toggleMenu()} aria-hidden="true">
+            <li className={`${styles['more-clickable-area']} ${styles['navbar-email']} ${styles.pointer}`} 
+            onClick={() => toggleMenu()} role="presentation"
+
+            >
               platzi@example.com
             </li>
-            <li className={styles['navbar-shopping-cart']} onClick={() => {}} onKeyPress={toggleOrder()} aria-hidden="true">
+            <li className={styles['navbar-shopping-cart']} 
+            onClick={() => toggleOrder()} role="presentation"
+
+            >
               <Image className={`${styles['more-clickable-area']} ${styles.pointer}`} src={shoppingCart} alt="shopping cart" />
               {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
             </li>
