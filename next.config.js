15142dec9bf638739d7c78e2d3eeb66ace1ee893
bjpc <brandonpineda4@gmail.com>
2022-05-09 15:25:42 -0600

    :art: [ADD] pages, link, seo, iamges, context

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    domains: ['placeimg.com', 'api.lorem.space', 'api.escuelajs.co'],
+  },
 }
 
 module.exports = nextConfig
